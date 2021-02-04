9dbe1cead1f3b1a0babf7f14703c91e1458e38b1
Maddie <55294289+plolil@users.noreply.github.com>
2021-02-04 20:39:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hey~")
+def GetNextAction(entity): #entity is any person
+    print()                 #entity will be an array including an ID and other factors such as held money, coords, etc.
+
+def GetNextTrades(market): #Will retrieve all trades that will be performed at a given market
+    print()
+
+def GetMarketPriceChange(market, itemID): #Will calculate the change in price of an item based on supply/demand at a given market
+    print()
